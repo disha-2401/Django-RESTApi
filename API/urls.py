dea5e784187bcde84390343229e27cc13a7be7e4
disha-2401 <dishamiyani03@gmail.com>
2021-05-05 01:03:13 +0530

    indent changes

--- API/urls.py
+++ API/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-    
+
 urlpatterns = [
-    path('', views.apiOverview,name="api-overview"),
-    path('test',views.testView.as_view()),
-    path('BTS/<int:pk>',views.MemberDetails),
-    path('BTS',views.AllMemberDetails),
-    path('BTSCreate/',views.MemberCreate),
-    ]
+    path('', views.apiOverview, name="api-overview"),
+    path('test', views.testView.as_view()),
+    path('BTS/<int:pk>', views.MemberDetails),
+    path('BTS', views.AllMemberDetails),
+    path('BTSCreate/', views.MemberCreate),
+]
